86a3156a135ea4ac4b6a4aaa88f671a13be6f3cc
daitsu <daitsu@localhost.localdomain>
2017-04-15 17:53:30 -0400

    Primera version estable. Quizas habria que hacer cambias para eliminar lo del comando start, ya que si no empiezas con ese comando, no sigue el orden fundamental establecido para el usuario. Actualmente, almacena el ultimo comando utilizado en memoria dinamica. Seria interesante para el futuro implementar una base de datos sqlite en la que se pueda ir almacenando el historico y que al empezar con el caracter "/" te sugiera comandos del historico. Soporte para nano??

--- bot.py
+++ bot.py
@@ -1,37 +1,52 @@
-import tempfile
 import subprocess
-import os
 import telebot
 import config
-from telebot import types
 
 bot = telebot.TeleBot(config.apikey)
+users = {}
 
-teclado = types.ReplyKeyboardMarkup(one_time_keyboard=True)
-teclado.add('Potorro','Potorrin')
-teclado.add('cerrar')
-
-@bot.message_handler(commands=['hello'])
-def hola(message):
-    bot.send_message(message.chat.id, 'hello', reply_markup=teclado)
-
-@bot.message_handler(func=lambda m: m.text=='Potorro')
-def miau(m):
-    bot.send_message(m.chat.id, 'Has seleccionado Potorro')
+@bot.message_handler(commands=['start'])
+def hello_user(m):
+    if m.chat.id not in users.keys():
+        users[m.chat.id] = {'username': m.chat.username}
+    bot.send_message(m.chat.id, 'Welcome user. This bot will help you to manage a server running\
+    shell comands througt it.\n If you experiment any issue or have\
+    any suggestion, just let me know.\n Made with <3 @Joex23.')
+    print(users)
+            
+@bot.message_handler(commands=['repeat_command'])
+def repeat_command(m):
+    print(users)
+    try:
+        m.text = users[m.chat.id]['last_command']
+        execute_command(m)
+    except:
+        bot.send_message(m.chat.id, 'You haven\'t executed any command yet.')
 
 @bot.message_handler(func=lambda m: True)
-def miau(m):
+def execute_command(m):
+#    print(m)
+    print(users)
     if m.chat.id!=config.myid:
         return
-    command=m.text.split('/')[1]
-    print(command)
-    result=subprocess.run(command + '> tmp', shell=True, stdout=subprocess.PIPE)
-    bot.send_message(m.chat.id, '```'+result.stdout.decode('utf-8')+'```', parse_mode='Markdown')
-    return
-    for line in open('tmp'):
-        if len(line)>1:
-            bot.send_message(m.chat.id, '```'+line+'```', parse_mode='Markdown')
-        else:
-            continue
+    try:
+        users[m.chat.id]['last_command'] = m.text
+        command=m.text.split('/')[1]
+        print(command)
+    except:
+        bot.send_message(m.chat.id, 'Commands should start with "/"')
+        return    
+    result=subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    print(result.returncode)
+    print(result.stdout)
+    print(result.stderr)
+    if result.returncode is 0:
+        bot.send_message(m.chat.id, '```\n'+result.stdout.decode('utf-8')+'```', parse_mode='Markdown')
+    else:
+        try:
+            bot.send_message(m.chat.id, '```\n'+result.stderr.decode('utf-8')+'```', parse_mode='Markdown')
+        except:
+            bot.send_message(m.chat.id, '`Your command didn\'t return nothing.`', parse_mode='Markdown')
+
 print('Running')
 bot.polling()
